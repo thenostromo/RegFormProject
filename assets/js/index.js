131492e59b09ebb2480d87ceea658676f8b7f4b0
Alexander <redlesleys@gmail.com>
2019-10-20 12:17:36 +0300

    Develop

---
+++ assets/js/index.js
@@ -0,0 +1,2 @@
+import "../css/styles.css";
+console.log(123);
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,47 @@
-var path = require('path');
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     entry: './assets/js/index.js',
     output: {
-        filename: 'bundle.js',
+        filename: 'app.js',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    {
+                        loader: "css-loader",
+                    },
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            implementation: require("sass")
+                        }
+                    }
+                ]
+            }
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "styles.css"
+        })
+    ]
 };
